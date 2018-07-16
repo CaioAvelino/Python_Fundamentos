c0b7350b7c2c746c08adb69c09de1882f42e813b
CaioAvelino <caioavelino.eng@gmail.com>
2018-07-16 17:43:53 -0300

    Lab02_completed

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,3 +6,22 @@
 
 print("\n******************* Python Calculator *******************")
 
+option = int(input("\n\nSelecione o número da operação desejada: \n\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n\nDigite sua opção (1/2/3/4): "))
+primeiro = int(input("\n\nDigite o primeiro número: "))
+segundo = int(input("\n\nDigite o segundo número: "))
+
+if(option not in [1,2,3,4] or (option == 4 and segundo == 0)):
+    print("\n\nOpção inválida!")
+elif(option == 1):
+    soma = primeiro + segundo
+    print("\n\nSoma: %d + %d = %d" %(primeiro, segundo, soma))
+elif(option == 2):
+    sub = primeiro - segundo
+    print("\n\nSubtração: %d - %d = %d" %(primeiro, segundo, sub))
+elif(option == 3):
+    mult = primeiro * segundo
+    print("\n\nMultiplicação: %d * %d = %d" %(primeiro, segundo, mult))
+elif(option == 4 and segundo != 0):
+    div = float(primeiro / segundo)
+    print("\n\nDivisão: %d / %d = %f" %(primeiro, segundo, div))
+ 
